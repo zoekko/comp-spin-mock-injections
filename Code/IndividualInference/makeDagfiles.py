d8afcb93d89937d4612eb859d7a5a78d173a39b2
Simona Miller <simona.miller@ldas-pcdev2.ligo.caltech.edu>
2023-01-19 22:32:40 -0800

    test run of make dag files

--- Code/IndividualInference/makeDagfiles.py
+++ Code/IndividualInference/makeDagfiles.py
@@ -13,7 +13,8 @@ root = '/home/simona.miller/comp-spin-mock-injections/'
 
 # cycle through populations
 pop_names = ['population1_highSpinPrecessing', 'population2_mediumSpin', 'population3_lowSpinAligned']
-for pop_name in pop_names:
+pop_names_zoe = ['pop1', 'pop2', 'pop3']
+for j,pop_name in enumerate(pop_names):
 
     # Load in injected population
     injections = pd.read_json(f'../../Data/InjectedPopulationParameters/{pop_name}.json')
@@ -22,11 +23,10 @@ for pop_name in pop_names:
 
     # Choose random set to inject
     to_inject = np.random.choice(range(n_total),size=int(nevents),replace=False)
-    np.savetxt(f'injlist_{pop_name}_{nevents}events.txt',to_inject,fmt="%d")
 
-    # Write dag file
+    # Write dag file in the condor subfolder
     dagfile=f'./condor/bilby_{pop_name}.dag'
     with open(dagfile,'w') as df: 
         for i in to_inject:
-            df.write('JOB {0} {1}/Code/IndividualInference/condor/bilby_{1}.sub\n'.format(i,root,pop_name))
-            df.write('VARS {0} jobNumber="{0}" json="{1}/Data/InjectedPopulationParameters/{2}.json" outdir="{1}/Data/IndividualInferenceOutput/{2}/"\n\n'.format(i,root,pop_name))
+            df.write('JOB {0} {1}Code/IndividualInference/condor/bilby_{2}.sub\n'.format(int(i),root,pop_name))
+            df.write('VARS {0} jobNumber="{0}" json="{1}Data/InjectedPopulationParameters/{2}.json" outdir="{1}Data/IndividualInferenceOutput/{2}/"\n\n'.format(int(i),root,pop_name))
