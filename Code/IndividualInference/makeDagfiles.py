14225b9d16ad001afee4440e239e578316561115
Simona Miller <simona.miller@ldas-pcdev2.ligo.caltech.edu>
2023-01-19 22:13:45 -0800

    initial push for code to generate underlying populations and launch bilby

---
+++ Code/IndividualInference/makeDagfiles.py
@@ -0,0 +1,32 @@
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from scipy.special import erf
+import sys
+import pandas as pd
+
+# Pass number of of events to inject via commandline 
+nevents = sys.argv[1]
+
+# repository root
+root = '/home/simona.miller/comp-spin-mock-injections/'
+
+# cycle through populations
+pop_names = ['population1_highSpinPrecessing', 'population2_mediumSpin', 'population3_lowSpinAligned']
+for pop_name in pop_names:
+
+    # Load in injected population
+    injections = pd.read_json(f'../../Data/InjectedPopulationParameters/{pop_name}.json')
+    injections.sort_index(inplace=True)
+    n_total = len(injections)
+
+    # Choose random set to inject
+    to_inject = np.random.choice(range(n_total),size=int(nevents),replace=False)
+    np.savetxt(f'injlist_{pop_name}_{nevents}events.txt',to_inject,fmt="%d")
+
+    # Write dag file
+    dagfile=f'./condor/bilby_{pop_name}.dag'
+    with open(dagfile,'w') as df: 
+        for i in to_inject:
+            df.write('JOB {0} {1}/Code/IndividualInference/condor/bilby_{1}.sub\n'.format(i,root,pop_name))
+            df.write('VARS {0} jobNumber="{0}" json="{1}/Data/InjectedPopulationParameters/{2}.json" outdir="{1}/Data/IndividualInferenceOutput/{2}/"\n\n'.format(i,root,pop_name))
