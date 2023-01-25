91781912540c9b7b48175ece343faad62cfc116c
Zoe Ko <zoe.ko@ldas-pcdev2.ligo.caltech.edu>
2023-01-24 18:42:01 -0800

    updated figure 4

--- Code/PopulationInference/posterior_reweighting.py
+++ Code/PopulationInference/posterior_reweighting.py
@@ -13,10 +13,10 @@ from posterior_helper_functions import *
 
 pop = 'pop1'
 num_injections = '70'
-model = 'gaussianPlusGaussian' # either gaussianPlusGaussian or betaPlusDoubleGaussian
+model = 'betaPlusGaussian' # either betaPlusGaussian or betaPlusDoubleGaussian # OLD: betaPlusGaussian
 
 """
-Function to do posterior reweighting for beta plus double Gaussain model 
+Function to do posterior reweighting for beta plus Gaussain model 
     sampleDict = dictionary containing individual event samples
     hyperPEDict = dictionary containing hyperparemeter samples: from json file that emcee outputs: 
         samples are the individual curves from the final trace plots (less than the actual number of samples
@@ -35,15 +35,24 @@ def pop_reweight(sampleDict, hyperPEDict):
         print(f"event {k+1} of {len(sampleDict)}: {event}")
         
         # Unpack posterior samples for this event
-        chi1_samples = sampleDict[event]['a1']
-        chi2_samples =  sampleDict[event]['a2']
-        cost1_samples = sampleDict[event]['cost1']
-        cost2_samples = sampleDict[event]['cost2']
-        m1_samples = sampleDict[event]['m1']
-        m2_samples = sampleDict[event]['m2']
-        z_samples = sampleDict[event]['z']
-        z_prior_samples = sampleDict[event]['z_prior']
-        dVdz_samples = sampleDict[event]['dVc_dz']
+        chi1_samples = np.asarray(sampleDict[event]['a1'])
+        chi2_samples =  np.asarray(sampleDict[event]['a2'])
+        cost1_samples = np.asarray(sampleDict[event]['cost1'])
+        cost2_samples = np.asarray(sampleDict[event]['cost2'])
+        m1_samples = np.asarray(sampleDict[event]['m1'])
+        m2_samples = np.asarray(sampleDict[event]['m2'])
+        z_samples = np.asarray(sampleDict[event]['z'])
+        dVdz_samples = np.asarray(sampleDict[event]['dVc_dz'])
+        
+        # # Unpack posterior samples for this event
+        # chi1_samples = sampleDict[event]['a1']
+        # chi2_samples =  sampleDict[event]['a2']
+        # cost1_samples = sampleDict[event]['cost1']
+        # cost2_samples = sampleDict[event]['cost2']
+        # m1_samples = sampleDict[event]['m1']
+        # m2_samples = sampleDict[event]['m2']
+        # z_samples = sampleDict[event]['z']
+        # dVdz_samples = sampleDict[event]['dVc_dz']
 
         # indices corresponding to each sample for these events (will be used below in the for loop)
         nSamples = len(chi1_samples)
@@ -76,6 +85,24 @@ def pop_reweight(sampleDict, hyperPEDict):
                 p_chi2 = calculate_Gaussian_1D(chi2_samples, mu_chi, sigma_chi, 0, 1)
                 p_cost1 = calculate_Gaussian_1D(cost1_samples, mu_cost, sigma_cost, -1, 1)
                 p_cost2 = calculate_Gaussian_1D(cost2_samples, mu_cost, sigma_cost, -1, 1)
+            
+            elif model == 'betaPlusGaussian':
+                # Fetch i^th hyper PE sample: 
+                mu_chi = hyperPEDict['mu_chi']['processed'][i]
+                sigma_chi = hyperPEDict['sigma_chi']['processed'][i]
+                mu_cost = hyperPEDict['mu_cost']['processed'][i]
+                sigma_cost = hyperPEDict['sigma_cost']['processed'][i]
+                Bq = hyperPEDict['Bq']['processed'][i]
+
+                # Translate mu_chi and sigma_chi to beta function parameters a and b 
+                # See: https://en.wikipedia.org/wiki/Beta_distribution#Mean_and_variance
+                a, b = mu_sigma2_to_a_b(mu_chi, sigma_chi**2.)
+
+                # Evaluate model at the locations of samples for this event
+                p_chi1 = calculate_betaDistribution(chi1_samples, a, b)
+                p_chi2 = calculate_betaDistribution(chi2_samples, a, b)
+                p_cost1 = calculate_Gaussian_1D(cost1_samples, mu_cost, sigma_cost, -1, 1)
+                p_cost2 = calculate_Gaussian_1D(cost2_samples, mu_cost, sigma_cost, -1, 1)
                 
             elif model == 'betaPlusDoubleGaussian':
                 # Fetch i^th hyper PE sample: 
@@ -93,8 +120,8 @@ def pop_reweight(sampleDict, hyperPEDict):
                 a, b = mu_sigma2_to_a_b(mu_chi, sigma_chi**2.)
 
                 # Evaluate model at the locations of samples for this event
-                p_chi1 = betaDistribution(chi1_samples, a, b)
-                p_chi2 = betaDistribution(chi2_samples, a, b)
+                p_chi1 = calculate_betaDistribution(chi1_samples, a, b)
+                p_chi2 = calculate_betaDistribution(chi2_samples, a, b)
                 p_cost1 = calculate_Double_Gaussian(cost1_samples, mu1_cost, sigma1_cost, mu2_cost, sigma2_cost, MF_cost, -1, 1)
                 p_cost2 = calculate_Double_Gaussian(cost2_samples, mu1_cost, sigma1_cost, mu2_cost, sigma2_cost, MF_cost, -1, 1)
             
@@ -106,14 +133,24 @@ def pop_reweight(sampleDict, hyperPEDict):
             nSamples = pSpins.size
             spin_PE_prior = np.ones(nSamples)
             
+            # # Need to reweight by astrophysical priors on m1, m2, z ...
+            # # - p(m1)*p(m2)
+            # p_astro_m1_m2 = p_astro_masses(m1_samples, m2_samples, bq=Bq)
+            # old_m1_m2_prior = np.ones(nSamples) # PE prior on masses is uniform in component masses
+            # # - p(z)
+            # p_astro_redshift = p_astro_z(z_samples, dVdz_samples)
+            # # - For full m1, m2, z prior reweighting: 
+            # m1_m2_z_prior_ratio = (p_astro_m1_m2/old_m1_m2_prior)*(p_astro_redshift/z_prior_samples)
+            
             # Need to reweight by astrophysical priors on m1, m2, z ...
             # - p(m1)*p(m2)
             p_astro_m1_m2 = p_astro_masses(m1_samples, m2_samples, bq=Bq)
-            old_m1_m2_prior = np.ones(nSamples) # PE prior on masses is uniform in component masses
+            old_m1_m2_prior = np.power(1.+z_samples, 2) # PE prior on masses is uniform in DETECTOR FRAME component masses
             # - p(z)
             p_astro_redshift = p_astro_z(z_samples, dVdz_samples)
+            old_z_prior = p_astro_z(z_samples, dVdz_samples, kappa=0) # see: bilby.gw.prior.UniformSourceFrame prior 
             # - For full m1, m2, z prior reweighting: 
-            m1_m2_z_prior_ratio = (p_astro_m1_m2/old_m1_m2_prior)*(p_astro_redshift/z_prior_samples)
+            m1_m2_z_prior_ratio = (p_astro_m1_m2/old_m1_m2_prior)*(p_astro_redshift/old_z_prior) 
             
             # calculate weights for this hyper parameter
             weights = pSpins*m1_m2_z_prior_ratio/spin_PE_prior
@@ -192,7 +229,7 @@ if __name__=="__main__":
     sampleDict = json.load(f) 
     
     # Load population parameter PE samps
-    with open(f'/home/simona.miller/comp-spin-mock-injections/Data/PopulationInferenceOutput/gaussianPlusGaussian/gaussianPlusGaussian_{pop}_70events.json', 'r') as f:
+    with open(f'/home/simona.miller/comp-spin-mock-injections/Data/PopulationInferenceOutput/{model}/{model}_{pop}_{num_injections}events_temp.json', 'r') as f:
         hyperPEDict = json.load(f)
     
     # Run reweighting for default model
@@ -200,6 +237,7 @@ if __name__=="__main__":
     sampleDict_rw = pop_reweight(sampleDict, hyperPEDict)   
     
     # Save results
+    froot = '/home/zoe.ko/comp-spin-mock-injections/'
     savename = f'{froot}Data/PopulationInferenceOutput/{model}/70{model}_{pop}rw_sampleDict.pickle'
     with open(savename, 'wb') as f:
         pickle.dump(sampleDict_rw,f)
